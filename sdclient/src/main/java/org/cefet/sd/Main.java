ab993dfe636f17dd57b3832cc4b5a79e763d9936
gddeazevedo <gabrielcapuerj@gmail.com>
2025-11-30 22:21:55 -0300

    init client

---
+++ sdclient/src/main/java/org/cefet/sd/Main.java
@@ -0,0 +1,9 @@
+package org.cefet.sd;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        IO.println("Hello world");
+    }
+}
